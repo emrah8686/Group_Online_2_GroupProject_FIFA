c378366d4c14ed49c38a4155b76f25844c26ac7b
mgunduz72 <gunduztx@gmail.com>
2018-07-30 16:09:01 -0500

    first commit

--- src/test/java/com/fifa/pages/PlayersGoalScoredPage.java
+++ src/test/java/com/fifa/pages/PlayersGoalScoredPage.java
@@ -1,5 +1,6 @@
 package com.fifa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -13,7 +14,50 @@ public class PlayersGoalScoredPage {
 
 	@FindBy(tagName= "title")
 	public WebElement pageTitle;
-
-
+	
+	@FindBy(xpath= "//ul[@class='fi-section-header__nav']/li/a[.='Statistics']")
+	public WebElement st;
+	
+	@FindBy(linkText= "All Scorers")
+	public WebElement allScorers;
+	
+	@FindBy(xpath= "//div[@class='fi-boxed-page']/div/ul/li[2]/a")
+	public WebElement topSaves;
+	
+	@FindBy(xpath= "//div[@class='fi-boxed-page']/div/ul/li[4]/a")
+	public WebElement disciplinary;
+	
+	
+	
+	//table/tbody/tr[3]
+	
+	public WebElement playerName(int row){
+		return Driver.getDriver().findElement(By.xpath("//tbody/tr["+row+"]/td[2]"));
+		
+	}
+	
+	public WebElement scoreLeftFoot(int row){
+		return Driver.getDriver().findElement(By.xpath("//tbody/tr["+row+"]/td[8]"));
+		
+	}
+	
+	public WebElement scoreRightFoot(int row){
+		return Driver.getDriver().findElement(By.xpath("//tbody/tr["+row+"]/td[9]"));
+		
+	}
+	public WebElement head(int row){
+		return Driver.getDriver().findElement(By.xpath("//tbody/tr["+row+"]/td[10]"));
+		
+	}
+	
+	public String playerInfo(int num) {
+		
+		 String info = playerName(num).getText() + " " + scoreLeftFoot(num).getText()
+				 +" "+scoreRightFoot(num).getText()+" "+head(num).getText();
+		 return info;
+		
+	}
+	
+	
 
 }
